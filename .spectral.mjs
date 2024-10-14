3510f4620731ef33102d629341686ca8a31143c8
marcos.vieira@camunda.com <marcos.vieira@camunda.com>
2024-10-14 11:08:29 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2MTMzOQ/spectral.js?branch_slug=main&token=33a9f5ca-4694-43f6-8ee6-713ff6615329";
+export default { extends: ruleset };
